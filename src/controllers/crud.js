65a2188aac103d6268ce67b261ede10d2d874cc5
Caio Braga <caioaugustobrg@gmail.com>
2023-04-18 00:57:39 -0300

    :sparkles: feat: replace chilli to be the put method

--- src/controllers/crud.js
+++ src/controllers/crud.js
@@ -1,8 +1,25 @@
-module.exports = async (req,res) =>  {
-	let chillies = [];
-	let count = 0;
+let chillies = [];
+let count = 0;
+const addChilli = (req,res) =>  {
+	
 	const newChilli = { id: count, ...req.body };
 	count = count + 1;
 	chillies.push(newChilli);
 	res.send(newChilli);
+	console.log('add a chilli');
 };
+const replaceChilli = (req,res) => {
+	let chilliIndex = chillies.findIndex((c) => c.id == req.params.id);
+	// if no id was found send chilliMatch will be undefined which is falsy
+	if (chilliIndex != -1) {
+		chillies[chilliIndex] = req.body;
+		console.log(req.body);
+		res.send(chillies[chilliIndex]);
+	} else {
+		res.sendStatus(400);
+	}
+};
+
+
+
+module.exports = replaceChilli, addChilli;
