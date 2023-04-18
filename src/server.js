ad8da104f6e970610c12a87cc58ca533dfe78926
Caio Braga <caioaugustobrg@gmail.com>
2023-04-18 00:36:56 -0300

    :sparkles: feat: imported and connected cors

--- src/server.js
+++ src/server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const port = 3030;
+const cors = require('cors');
 const router = require('./routes/index');
+
+
+app.use(cors());
 app.use(router);
 app.listen(port, () => {
 	console.log(`Express running on http://localhost:${port}; press CRTL + C to terminate...` );
