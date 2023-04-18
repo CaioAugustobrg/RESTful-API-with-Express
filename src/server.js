dac167ca4fde21dfb794dc5a80e27e368bd655d8
Caio Braga <caioaugustobrg@gmail.com>
2023-04-18 00:33:34 -0300

    :sparkles: feat: imported routes file

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const port = 3030;
-
+const router = require('./routes/index');
+app.use(router);
 app.listen(port, () => {
 	console.log(`Express running on http://localhost:${port}; press CRTL + C to terminate...` );
 });
